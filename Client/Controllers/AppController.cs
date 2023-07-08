7746eeff5a3c46e30850f84966968ebda72f753b
Marc <marc@interactivedrift.com>
2023-07-08 16:02:48 -0700

    scaffolded angular client, updated to latest core

---
+++ Client/Controllers/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AppController : ControllerBase
+{
+    private readonly ILogger<AppController> _logger;
+
+    public AppController(ILogger<AppController> logger)
+    {
+        _logger = logger;
+    }
+}
